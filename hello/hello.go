6ac02511aab04e94070dbff999d2b620f0b2954e
kkruups <kkruups@gmail.com>
2015-10-28 23:13:53 +0900

    new

---
+++ hello/hello.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+          "fmt"
+          "github.com/kkruups/stringutil"
+)
+
+
+func main(){
+
+    fmt.Printf("hello, world\n")
+    fmt.Printf(stringutil.Reverse("!oG, olleH"))
+    fmt.Printf("\n")
+
+}
+
